9b89e57d6a4cb0bd3dbfd410d33bc74bcee13bf9
raje2411 <rraman@hw12194.lan>
2018-08-30 05:48:01 -0400

    Improvement of ConstInheritenceShape2Demo,  here we have added many different constructors for both super class and sub-class

--- Java_inheritence/src/inheritence/ConstInheritenceShape3Demo.java
+++ Java_inheritence/src/inheritence/ConstInheritenceShape3Demo.java
@@ -1,10 +1,8 @@
-/* This program is to give demo on Constructors & inheritence.
+/* Improvement of ConstInheritenceShape2Demo,  here we have added many different constructors for both super class and sub-class
+ * This program is to give demo on Constructors & inheritence.
  * We are going to copy the code from "SuperClassPrivateMembersAccesDemo.Java".   
  * TwoDShapeNew(super - adding its own constructor)-->TwoDShapeNew2  & TriangleNew(sub copied to TriangleNewShape1)-->Triangle Classes created in "SuperClassPrivateMembersAccesDemo.Java"
  * In this demo we will have constructor only on the sub-class(TriangleNew) and use super() to construct the super class members
- * 
- * Improvement of ConstInheritenceShape2Demo,  here we have added many different constructors for both super class and sub-class
- *
  */
 
 package inheritence;
@@ -15,18 +13,18 @@ class TwoDShapeNew3 { // Superclass
 							// getWidth() & setWidth()
 	private double height; // Now these are private and subclasses can access these only through methods
 							// getHeight() & setHeight()
-	
-	TwoDShapeNew3(){
-		width=height=0;
+
+	TwoDShapeNew3() {
+		width = height = 0;
 	}
-	
+
 	TwoDShapeNew3(double w, double h) { // Now created this super class constructor on this TwoDShapeNew2 class
 		width = w;
 		height = h;
 	}
-	
-	TwoDShapeNew3(double x){
-		width=height=x;
+
+	TwoDShapeNew3(double x) {
+		width = height = x;
 	}
 
 	double getWidth() {
@@ -53,12 +51,12 @@ class TwoDShapeNew3 { // Superclass
 class TriangleNewShape3 extends TwoDShapeNew3 { // Sub-class which is an extension of TwoDShapeNew2 super class
 
 	String style;
-	
-	TriangleNewShape3(){
+
+	TriangleNewShape3() {
 		super();
 		style = "no style defined";
 	}
-	
+
 	TriangleNewShape3(String s, double w, double h) { // Defined constructor for sub-class only
 
 		super(w, h); // using super() keywords to invoke the super-class i.e TwoDShapeNew2
@@ -68,15 +66,11 @@ class TriangleNewShape3 extends TwoDShapeNew3 { // Sub-class which is an extensi
 		style = s;
 
 	}
-	
-	TriangleNewShape3(String s, double x)
-	{
-		super(x);
-		style =s;
-	}
-	
-	
 
+	TriangleNewShape3(String s, double x) {
+		super(x);
+		style = s;
+	}
 
 	double area() {
 		return (getWidth() * getHeight() * 1 / 2); // Now it can't access the super classes member directly.so using
@@ -95,9 +89,9 @@ public class ConstInheritenceShape3Demo {
 		TriangleNewShape3 obj_tglnewshp_1 = new TriangleNewShape3();
 		TriangleNewShape3 obj_tglnewshp_2 = new TriangleNewShape3("outlined", 250, 30);
 		TriangleNewShape3 obj_tglnewshp_3 = new TriangleNewShape3("semifilled", 30);
-		TriangleNewShape3[] arry = { obj_tglnewshp_1, obj_tglnewshp_2,obj_tglnewshp_3 };
-		
-		for ( TriangleNewShape3 i : arry) {
+		TriangleNewShape3[] arry = { obj_tglnewshp_1, obj_tglnewshp_2, obj_tglnewshp_3 };
+
+		for (TriangleNewShape3 i : arry) {
 			System.out.println("+++++++++++");
 			System.out.println("Objects dimension");
 			System.out.println("");
