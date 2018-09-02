6fce6b8e400463772eefe83d97fc75bcd54d6203
raje2411 <rraman@hortonworks.com>
2018-09-01 21:51:09 -0400

    first intellij commit

--- Java_inheritence/src/inheritence/AcessSuperMemberDemo.java
+++ Java_inheritence/src/inheritence/AcessSuperMemberDemo.java
@@ -3,53 +3,55 @@
  */
 package inheritence;
 
- class A {
-	int i;
-	int get_var(){
-		return i;
-	}
-	void set_var(int x){
-		i=x;
-	}
+class A {
+    int i;
+
+    int get_var() {
+        return i;
+    }
+
+    void set_var(int x) {
+        i = x;
+    }
 }
 
 class B extends A {
-	int i;
+    int i;
 
-	B(int x, int y) {
-		set_var(y);
-		i = x;
-	}
+    B(int x, int y) {
+        set_var(y);
+        i = x;
+    }
 
-	B(int x) {
-		set_var(x);
-		i = x;
-	}
+    B(int x) {
+        set_var(x);
+        i = x;
+    }
 
-	B() {
-		set_var(0);
-		i  = 0;
-	}
+    B() {
+        set_var(0);
+        i = 0;
+    }
 
-	void show_var() {
-		System.out.println("Class A's i value is : " + super.i);
-		System.out.println("Class B's i value is : " + i);
-	}
+    void show_var() {
+        System.out.println("Class A's i value is : " + super.i);
+        System.out.println("Class B's i value is : " + i);
+    }
 }
 
 public class AcessSuperMemberDemo {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		B obj_B = new B(5, 10); // here 10 is for class A's i and 5 is for class B
-		B obj_B1 = new B(33); // here 33 is for class A's i and 33 is for class B
-		B obj_B2 = new B(); // here 33 is for class A's i and 33 is for class B
+        B obj_B = new B(5, 10); // here 10 is for class A's i and 5 is for class B
+        B obj_B1 = new B(33); // here 33 is for class A's i and 33 is for class B
+        B obj_B2 = new B(); // here 33 is for class A's i and 33 is for class B
 
-		obj_B.show_var();
-		System.out.println("++++++++++++++++++++++++++");
-		obj_B1.show_var();
-		System.out.println("++++++++++++++++++++++++++");
-		obj_B2.show_var();
-	}
+        obj_B.show_var();
+        System.out.println("++++++++++++++++++++++++++");
+        obj_B1.show_var();
+        System.out.println("++++++++++++++++++++++++++");
+        obj_B2.show_var();
+    }
 
 }
--- Java_inheritence/src/inheritence/SuperClassRefAndSubClassObjDemo.java
+++ Java_inheritence/src/inheritence/SuperClassRefAndSubClassObjDemo.java
@@ -4,51 +4,51 @@ package inheritence;
 
 class SuperClass {
 
-	int x;
+    int x;
 
-	SuperClass(int i) {
-		x = i;
+    SuperClass(int i) {
+        x = i;
 
-	}
+    }
 
 }
 
 class SubClass extends SuperClass {
-	int y;
+    int y;
 
-	SubClass(int i, int j) {
-		super(i);
-		y = j;
-	}
+    SubClass(int i, int j) {
+        super(i);
+        y = j;
+    }
 
 }
 
 public class SuperClassRefAndSubClassObjDemo {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SuperClass obj_super = new SuperClass(5);
-		SuperClass obj_super_class_ref;
-		SubClass obj_sub = new SubClass(15, 20);
+        SuperClass obj_super = new SuperClass(5);
+        SuperClass obj_super_class_ref;
+        SubClass obj_sub = new SubClass(15, 20);
 
-		// Case1: This is valid as both the objects obj_super and reference obj
-		// obj_super_class_ref, both are of the same type SuperClass.
-		obj_super_class_ref = obj_super;
-		System.out.println("obj_super_class_ref object is referring to " + obj_super_class_ref.getClass());
-		System.out.println("Accessing obj_super 'x' value : " + obj_super_class_ref.x);
+        // Case1: This is valid as both the objects obj_super and reference obj
+        // obj_super_class_ref, both are of the same type SuperClass.
+        obj_super_class_ref = obj_super;
+        System.out.println("obj_super_class_ref object is referring to " + obj_super_class_ref.getClass());
+        System.out.println("Accessing obj_super 'x' value : " + obj_super_class_ref.x);
 
-		System.out.println("");
+        System.out.println("");
 
-		// Case2: This is also valid, as obj_super_class_ref is of type superclass which
-		// is superior to subclass. but there is a catch.
-		obj_super_class_ref = obj_sub;
-		System.out.println("obj_super_class_ref object is NOW referring to " + obj_super_class_ref.getClass());
-		System.out.println("Accessing obj_super 'x' value : " + obj_super_class_ref.x); // This is good.
-		System.out.println("Accessing obj_super 'y' value : " + obj_sub.y); // only access through obj_sub
-		// This won't work if we use "obj_super_class_ref.y" in the above println -
-		// because obj_super_class_ref doesn't know it subclass
-		// objects members.
+        // Case2: This is also valid, as obj_super_class_ref is of type superclass which
+        // is superior to subclass. but there is a catch.
+        obj_super_class_ref = obj_sub;
+        System.out.println("obj_super_class_ref object is NOW referring to " + obj_super_class_ref.getClass());
+        System.out.println("Accessing obj_super 'x' value : " + obj_super_class_ref.x); // This is good.
+        System.out.println("Accessing obj_super 'y' value : " + obj_sub.y); // only access through obj_sub
+        // This won't work if we use "obj_super_class_ref.y" in the above println -
+        // because obj_super_class_ref doesn't know it subclass
+        // objects members.
 
-	}
+    }
 
 }
